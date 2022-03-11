15621128e6811a69b24015b2fd5e923dfc5e5467
Charlan Barneche <97557224+Charlan-Barneche@users.noreply.github.com>
2022-03-11 19:13:04 -0300

    v1.1

--- index.js
+++ index.js
@@ -2,8 +2,3 @@ let menu = document.querySelector ('.menu');
     menu.onclick = function (){
         menu.classList.toggle('active')
     }
-
-let title = document.querySelector ('.title');
-    title.onclick = function (){
-        title.classList.toggle ('active')
-    }
