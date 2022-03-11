f167c35f3d585bc4bf869b7cdcb83a742e211360
Charlan Barneche <97557224+Charlan-Barneche@users.noreply.github.com>
2022-03-11 17:54:26 -0300

    menu interativo v1.0

---
+++ index.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector ('.menu');
+    menu.onclick = function (){
+        menu.classList.toggle('active')
+    }
+
+let title = document.querySelector ('.title');
+    title.onclick = function (){
+        title.classList.toggle ('active')
+    }
